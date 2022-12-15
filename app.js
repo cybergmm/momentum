990a050e53ca338c232f83b7cf7dd935062f5d95
유수민 <yoo@Sumins-Macbook.local>
2022-12-15 23:16:09 +0900

    good

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-function sayHello(name) {
-    console.log("Hello my name is "+name);
+const calculater = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    multiple: function (a, b) {
+        console.log(a * b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+    power: function (a, b) {
+        console.log(a ** b);
+    }
+    
 }
 
-sayHello("sumin");
-sayHello("dal")
-sayHello("lynn");
+calculater.add(2, 5);
+calculater.minus(7 , 3);
+calculater.multiple(3, 7);
+calculater.divide(6, 3);
+calculater.power(2, 8);
